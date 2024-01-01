218061a7d276916cf038a142f5efda7b1b2c619a
owaisezarger <owaisezarger@gmail.com>
2024-01-01 23:48:45 +0530

    complete: module-3

--- node/module-3/app.js
+++ node/module-3/app.js
@@ -1,3 +1,9 @@
 const math = require("./math.js");
 console.log(math.add(12, 7));
 console.log(math.sub(12, 7));
+
+const fs = require("fs");
+fs.readFile("./file.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
